87f791c2c276195ee2df01e1303e97acd0300e0d
Shaurya Jha <shauryajha35@gmail.com>
2024-12-01 02:15:57 +0530

    Add Explore Content

--- client/src/app/explore/page.jsx
+++ client/src/app/explore/page.jsx
@@ -3,7 +3,9 @@ import Navbar from "@/components/home/Navbar"
 export default function Home() {
   return (
     <>
-      <Navbar/>
+      <div className="min-h-screen bg-gray-100 dark:bg-gray-900">
+        <Navbar />
+      </div>
     </>
   );
 }
