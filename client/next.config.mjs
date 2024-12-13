3ab4dabba5e781fadf7a86ae30d4a962724a0fc2
Shaurya Jha <shauryajha35@gmail.com>
2024-12-14 00:08:01 +0530

    Test Prod

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,9 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['media.discordapp.net'], 
-    },
-  };
-  
-  export default nextConfig;
-  
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "https://iter-social-connect-sv78.onrender.com" }, // replace this your actual origin
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,DELETE,PATCH,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
+  images: {
+    domains: ["media.discordapp.net"],
+  },
+};
+
+export default nextConfig;
