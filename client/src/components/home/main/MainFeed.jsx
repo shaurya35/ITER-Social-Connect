3b459cf1ebd8d4e2b8e38abbd345bead761ca6f9
Shaurya Jha <shauryajha35@gmail.com>
2024-12-13 23:57:14 +0530

    Fix prod

--- client/src/components/home/main/MainFeed.jsx
+++ client/src/components/home/main/MainFeed.jsx
@@ -34,8 +34,6 @@ const timeAgo = (dateString) => {
   return `${years} years ago`;
 };
 
-axios.defaults.withCredentials = true;
-
 export default function MainFeed() {
   const [posts, setPosts] = useState([]);
   const [error, setError] = useState(null);
@@ -56,11 +54,10 @@ export default function MainFeed() {
     setLoading(true);
     try {
       // await new Promise(resolve => setTimeout(resolve, 10000));
-      const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/api/feed`, {
+      const response = await axios.get(`https://iter-social-connect-sv78.onrender.com/api/feed`, {
         params: { page, limit: 10 },
-        withCredentials: true, 
-      });      
-      console.log("API Response:", response.data);
+        withCredentials: true,
+      });
       const newPosts = response.data.posts || [];
       setPosts((prevPosts) => [...prevPosts, ...newPosts]);
       setHasMore(newPosts.length > 0);
