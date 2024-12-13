fd1a633fd1c8cb460b1d4c766b41b4d7a68d1bfb
Shaurya Jha <shauryajha35@gmail.com>
2024-12-14 00:20:30 +0530

    Test prod

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -3,11 +3,10 @@ const nextConfig = {
   async headers() {
     return [
       {
-        // matching all API routes
         source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "https://iter-social-connect-sv78.onrender.com" }, // replace this your actual origin
+          { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
           {
             key: "Access-Control-Allow-Methods",
             value: "GET,DELETE,PATCH,POST,PUT",
