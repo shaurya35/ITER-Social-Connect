cab88ba0fb93b924cc275f9c84c22469ecbd1f3b
Shaurya Jha <shauryajha35@gmail.com>
2024-12-14 01:41:20 +0530

    Add tests prod

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -10,7 +10,7 @@ const nextConfig = {
           // Allow for specific domains to have access or * for all
           {
             key: "Access-Control-Allow-Origin",
-            value: "https://itersocialconnect-backend.onrender.com",
+            value: "*",
             // DOES NOT WORK
             // value: process.env.ALLOWED_ORIGIN,
           },
--- client/src/components/home/main/MainFeed.jsx
+++ client/src/components/home/main/MainFeed.jsx
@@ -54,7 +54,7 @@ export default function MainFeed() {
     setLoading(true);
     try {
       // await new Promise(resolve => setTimeout(resolve, 10000));
-      const response = await axios.get(`https://itersocialconnect-backend.onrender.com/api/feed`, {
+      const response = await axios.get(`http://localhost:8080/api/feed`, {
         params: { page, limit: 10 },
         withCredentials: true,
       });
