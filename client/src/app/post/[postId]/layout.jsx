19a5198a135de086165c831b352f3779b256cdfc
Shaurya Jha <shauryajha35@gmail.com>
2025-02-01 04:36:21 +0530

    Add Favico.ico

---
+++ client/src/app/post/[postId]/layout.jsx
@@ -0,0 +1,10 @@
+export const metadata = {
+  title: "My Posts / ITER Connect",
+  description: "Connect, Collaborate, and Grow",
+};
+
+const PostsLayout = (props) => {
+  return <div>{props.children}</div>;
+};
+
+export default PostsLayout;
