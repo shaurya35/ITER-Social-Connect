666c72cc46a6513f1573365c025b8f4f8823a08e
OmShankarDeshmukh01 <omdeshmukhstudy@gmail.com>
2025-02-04 10:03:49 +0530

    [fix : fixed connections]

--- server/routes/connectionRoutes.js
+++ server/routes/connectionRoutes.js
@@ -4,6 +4,7 @@ const {
   sendConnectionRequest,
   getConnectionRequests,
   respondToConnectionRequest,
+  getAllConnections, // Add this new controller function
 } = require("../controllers/connectionControllers.js"); // Updated path for consistency
 const { isLoggedIn } = require("../middlewares/authMiddlewares.js"); // Updated path and fixed typo
 
@@ -13,4 +14,5 @@ router.get("/requests", isLoggedIn, getConnectionRequests);
 
 router.post("/respond", isLoggedIn, respondToConnectionRequest);
 
+router.get("/", isLoggedIn, getAllConnections); 
 module.exports = router;
