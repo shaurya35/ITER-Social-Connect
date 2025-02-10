df307fd96d3749b93174687c861bfaaf4d9f6d47
Shaurya Jha <shauryajha35@gmail.com>
2025-02-11 03:00:34 +0530

    Fix Tokens in Production

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -493,14 +493,6 @@ const logout = (req, res) => {
       // domain: process.env.NODE_ENV === "production" ? "your-backend-domain.com" : undefined,
     });
 
-    // res.clearCookie("refreshToken", {
-    //   httpOnly: true,
-    //   secure: process.env.NODE_ENV === "production",
-    //   sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-    //   path: "/",
-    //   domain: process.env.NODE_ENV === "production" ? ".vercel.app" : undefined,
-    // });
-
     res.status(200).json({ message: "Logged out successfully" });
   } catch (error) {
     console.error("Logout Error:", error);
