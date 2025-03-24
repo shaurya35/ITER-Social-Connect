bcd404d2da7d2789c3b27eebd8e73408088960bf
mic-720 <satyamksvc@gmail.com>
2025-03-24 11:02:51 +0530

    Minor Improvements

--- Supabase/server.js
+++ Supabase/server.js
@@ -8,4 +8,8 @@ app.use(express.json());
 app.use("/api", uploadRoutes);
 
 const PORT = process.env.PORT || 5000;
+app.get("/", (req, res) => {
+    res.send("Welcome to Supabase");
+});
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
